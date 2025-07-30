0b80ea600edb7a1710870c2e31816068c8dc6417
Jhgz16 <forthmonths@gmail.com>
2025-07-30 09:04:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'bg-remover-cache-v1';
 const urlsToCache = [
     './',
     './index.html',
+    './app.js',
     'https://cdn.jsdelivr.net/npm/react@18.2.0/umd/react.production.min.js',
     'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/umd/react-dom.production.min.js',
-    'https://cdn.jsdelivr.net/npm/babel-standalone@7.24.7/babel.min.js',
     'https://cdn.tailwindcss.com',
     'https://cdn.jsdelivr.net/npm/heic2any@0.0.4/dist/heic2any.min.js',
     'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.1/dist/transformers.min.js'
