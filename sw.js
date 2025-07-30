813e76ea0f747d791dd3965f9bd9ae2f687fa40f
Jhgz16 <forthmonths@gmail.com>
2025-07-30 09:39:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ const urlsToCache = [
     './app.js',
     'https://cdn.jsdelivr.net/npm/react@18.2.0/umd/react.production.min.js',
     'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/umd/react-dom.production.min.js',
-    'https://cdn.tailwindcss.com',
     'https://cdn.jsdelivr.net/npm/heic2any@0.0.4/dist/heic2any.min.js',
     'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.1/dist/transformers.min.js'
 ];
